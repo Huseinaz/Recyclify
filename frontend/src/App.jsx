bd3fa79eec8a10b3a0792a517b8a91cea539491a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-23 01:11:47 +0300

    Installing Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from './components/Authentication/LoginForm';
 
+import './output.css';
+
 const App = () => {
   return (
     <div className="app">
