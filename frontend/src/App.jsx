019c19c0dc457d740279aeffd61d1923532b4ceb
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-24 23:08:41 +0300

    Users Table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from './components/Authentication/LoginForm';
 import Sidebar from './components/Sidebar';
 import './style/colors.css'
+import UsersTable from './components/UsersTable';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/sidebar" element={<Sidebar />} />
+          <Route path='/userstable' element={<UsersTable />} />
         </Routes>
       </BrowserRouter>
     </div>
