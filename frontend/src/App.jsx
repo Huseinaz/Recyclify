efc125962736cc310e223eda1efb0f11f07aba44
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-24 23:26:19 +0300

    Drivers Table

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import LoginForm from './components/Authentication/LoginForm';
 import Sidebar from './components/Sidebar';
 import './style/colors.css'
 import UsersTable from './components/UsersTable';
+import DriversTable from './DriversTable';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<LoginForm />} />
           <Route path="/sidebar" element={<Sidebar />} />
           <Route path='/userstable' element={<UsersTable />} />
+          <Route path='/driverstable' element={<DriversTable />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/DriversTable/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const DriversTable = () => {
+    return (
+        <div className="overflow-x-auto">
+            <h1 className="text-xl font-bold pb-3">Drivers Information</h1>
+            <table className="table-auto min-w-full border-collapse border-t border-b border-gray-200">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 border-b border-gray-200 w-1/12">ID</th>
+                        <th className="px-4 py-2 border-b border-gray-200 w-2/12">Name</th>
+                        <th className="px-4 py-2 border-b border-gray-200 w-2/12">Email</th>
+                        <th className="px-4 py-2 border-b border-gray-200 w-2/12">Phone</th>
+                        <th className="px-4 py-2 border-b border-gray-200 w-2/12">Location</th>
+                        <th className="px-4 py-2 border-b border-gray-200 w-3/12">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">1</td>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">John Doe</td>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">john@mail.com</td>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">0123457</td>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">Beirut</td>
+                        <td className="px-4 py-2 border-b border-gray-200 text-center">
+                            <button className="bg-green-500 hover:bg-green-600 text-white font-bold py-1 px-2 rounded mr-2">
+                                Activate
+                            </button>
+                            <button className="bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-1 px-2 rounded">
+                                Shutdown
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default DriversTable;
