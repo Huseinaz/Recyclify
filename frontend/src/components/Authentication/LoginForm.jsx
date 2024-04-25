4b8aca20396aff3c8f62f466ac37a45dfe6e5bd7
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-25 22:27:48 +0300

    Editing login page

--- frontend/src/components/Authentication/LoginForm.jsx
+++ frontend/src/components/Authentication/LoginForm.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
 import './index.css';
 import logo from '../../assets/logo-bg-gray.png';
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8000/api/login', {
+                email,
+                password,
+            });
+            if (response.status === 200) {
+                localStorage.setItem('token', response.data.authorisation.token);
+                navigate('/dashboard');
+                return;
+            } else {
+                throw new Error();
+            }
+        } catch (error) {
+            setError('Wrong email or password');
+        }
+    };
+
     return (
         <div className="white flex items-center justify-center h-screen login-bg">
             <div className="gray-bg p-8 rounded-md shadow-lg max-w-xs w-full">
@@ -10,14 +38,33 @@ const LoginForm = () => {
                     <img src={logo} alt="Logo" className="w-32 h-32 object-cover" />
                 </div>
                 <h2 className="text-3xl font-bold text-center mb-10">Recyclify</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
-                        <input type="email" id="email" className="form-input w-full px-4 py-2 border rounded-lg text-gray-700" required placeholder="example@gmail.com" />
+                        <input
+                            type="email"
+                            id='email'
+                            value={email}
+                            className="form-input w-full px-4 py-2 border rounded-lg text-gray-700"
+                            required
+                            placeholder="example@gmail.com"
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="mb-6">
-                        <input type="password" id="password" className="form-input w-full px-4 py-2 border rounded-lg text-gray-700" required placeholder="••••••••" />
+                        <input
+                            type="password"
+                            value={password}
+                            className="form-input w-full px-4 py-2 border rounded-lg text-gray-700"
+                            required
+                            placeholder="••••••••"
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
-                    <button type="submit" className="w-full primary-bg text-white px-4 py-2 rounded-lg mb-4 login-btn">Log In</button>
+                    <button
+                        type="submit"
+                        className="w-full primary-bg text-white px-4 py-2 rounded-lg mb-4 login-btn">
+                        Log In
+                    </button>
                 </form>
             </div>
         </div>
