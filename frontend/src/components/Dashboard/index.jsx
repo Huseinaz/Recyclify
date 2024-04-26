6f68c4b35c2db0b974a43f4d6722963b426f1c48
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-26 23:05:00 +0300

    Activate users/drivers count

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Dashboard = () => {
-    const userCount = 100;
+    const [users, setUsers] = useState([]);
+    const [userCount, setUserCount] = useState(0);
+    const [driverCount, setDriverCount] = useState(0);
+
+    const fetchUsers = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/api/users/get', {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+
+            const allUsers = response.data.users;
+            setUsers(allUsers);
+
+            const userCount = allUsers.filter(user => user.role_id === 2).length;
+            const driverCount = allUsers.filter(user => user.role_id === 3).length;
+            setUserCount(userCount);
+            setDriverCount(driverCount);
+        } catch (error) {
+            console.error('Error fetching users:', error.response);
+        }
+    };
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
@@ -9,14 +36,13 @@ const Dashboard = () => {
                 <i className="fas fa-users text-primary-500 text-4xl mb-2"></i>
                 <span className="text-primary-500 text-4xl font-bold">{userCount}</span>
                 <p className="text-gray-700 mt-2 text-lg">Total Users</p>
-                <div className="mt-4 flex justify-center">
-                    <button className="px-4 py-2 bg-primary-200 text-primary-500 rounded-md hover:bg-primary-300 focus:outline-none">
-                        View Details
-                    </button>
-                </div>
+            </div>
+            <div className="bg-gradient-to-r from-primary-100 to-primary-200 rounded-lg shadow-md p-8 flex flex-col items-center justify-center hover:shadow-lg">
+                <i className="fas fa-users text-primary-500 text-4xl mb-2"></i>
+                <span className="text-primary-500 text-4xl font-bold">{driverCount}</span>
+                <p className="text-gray-700 mt-2 text-lg">Total Drivers</p>
             </div>
         </div>
-
     );
 };
 
