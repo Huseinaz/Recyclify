6d1fd515075a1e79d92771a9daedb38d97d95b57
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-30 01:00:26 +0300

    Profile picture and name

--- mobile/lib/pages/profile_page.dart
+++ mobile/lib/pages/profile_page.dart
@@ -7,7 +7,33 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       backgroundColor: Color(0xFFF3F5F8),
-      
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              
+              SizedBox(height: 60),
+              
+              CircleAvatar(
+                radius: 60,
+                backgroundImage: AssetImage('assets/image/logo-bg-green.png'),
+              ),
+
+              SizedBox(height: 10),
+
+              Text(
+                'John Doe',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
