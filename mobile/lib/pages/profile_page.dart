bde553e48b9ba4ece72b4464f08db79ca3b3750b
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-04-30 01:23:50 +0300

    Green background for profile

--- mobile/lib/pages/profile_page.dart
+++ mobile/lib/pages/profile_page.dart
@@ -5,31 +5,59 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFF3F5F8),
+    return Scaffold(
+      backgroundColor: const Color(0xFFF3F5F8),
       body: SafeArea(
         child: Center(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              
-              SizedBox(height: 60),
-              
-              CircleAvatar(
-                radius: 60,
-                backgroundImage: AssetImage('assets/image/logo-bg-green.png'),
-              ),
 
-              SizedBox(height: 10),
+              Expanded(
+                flex: 2,
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  
+                  child: Container(
+                    color: const Color(0xFF187B1B),
+                    padding: const EdgeInsets.all(16),
+                    
+                    child: const Column(
+                      
+                      children: [
+                        
+                        SizedBox(height: 35),
+                        
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage: AssetImage('assets/image/logo-bg-gray.png'),
+                        ),
 
-              Text(
-                'John Doe',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+                        SizedBox(height: 10),
+
+                        Text(
+                          'John Doe',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ),
 
+              Expanded(
+                flex: 3,
+                child: Container(
+                  
+                ),
+              ),
             ],
           ),
         ),
