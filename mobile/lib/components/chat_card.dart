029ca7a5afd6d0b67896503272e14a1b3ca3d2ee
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-01 10:54:43 +0300

    Updating chat card

--- mobile/lib/components/chat_card.dart
+++ mobile/lib/components/chat_card.dart
@@ -3,59 +3,63 @@ import 'package:flutter/material.dart';
 class ChatCard extends StatelessWidget {
   final Image profile;
   final String name;
-  final String message;
+  final String lastMessage;
   final String time;
 
-  const ChatCard({super.key, required this.message, required this.time, required this.profile, required this.name});
+  const ChatCard({
+    super.key,
+    required this.profile,
+    required this.name,
+    required this.lastMessage,
+    required this.time,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          CircleAvatar(
-            backgroundImage: profile.image,
-            radius: 24,
-          ),
-          SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CircleAvatar(
+          backgroundImage: profile.image,
+          radius: 25,
+        ),
+        const SizedBox(width: 10),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
                 ),
-                SizedBox(height: 4),
-                Text(
-                  message,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
-                  ),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+              ),
+              const SizedBox(height: 4),
+              Text(
+                lastMessage,
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.black,
                 ),
-              ],
-            ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ],
           ),
-          SizedBox(width: 10),
-          Text(
-            time,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w300,
-              color: Colors.grey,
-            ),
+        ),
+        const SizedBox(width: 10),
+        Text(
+          time,
+          style: const TextStyle(
+            fontSize: 12,
+            height: 2,
+            fontWeight: FontWeight.w300,
+            color: Colors.grey,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
