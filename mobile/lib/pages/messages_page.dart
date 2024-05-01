9c44256ce4188ddafb194d669b404a03f92f09ec
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-01 10:55:13 +0300

    Adding chat cards to messages page

--- mobile/lib/pages/messages_page.dart
+++ mobile/lib/pages/messages_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/components/chat_card.dart';
 
 class MessagesPage extends StatelessWidget {
   const MessagesPage({super.key});
@@ -10,18 +11,36 @@ class MessagesPage extends StatelessWidget {
       body: SafeArea(
         child: Container(
           margin: const EdgeInsets.only(left: 20, right: 20),
-          child: const Column(
+          child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 50),
-              Text(
+              const SizedBox(height: 50),
+              const Text(
                 'Messages',
                 style: TextStyle(
                   fontSize: 24,
                   fontWeight: FontWeight.w600,
                 ),
               ),
-              SizedBox(height: 40),
+              
+              const SizedBox(height: 40),
+              
+              ChatCard(
+                profile: Image.asset('assets/image/profile.png'),
+                name: 'John Doe',
+                lastMessage:'This is a message This is a message This is a message',
+                time: '12:00',
+              ),
+              
+              const SizedBox(height: 20),
+              
+              ChatCard(
+                profile: Image.asset('assets/image/profile.png'),
+                name: 'John Doe',
+                lastMessage: 'This is a message',
+                time: '12:00',
+              ),
+
             ],
           ),
         ),
