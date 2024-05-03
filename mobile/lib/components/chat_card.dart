e781c93a8894621c303b87a26399c0c7b008820d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-03 22:39:59 +0300

    Updating chat card

--- mobile/lib/components/chat_card.dart
+++ mobile/lib/components/chat_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/pages/chat_room_page.dart';
 
 class ChatCard extends StatelessWidget {
   final Image profile;
@@ -16,50 +17,63 @@ class ChatCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        CircleAvatar(
-          backgroundImage: profile.image,
-          radius: 25,
-        ),
-        const SizedBox(width: 10),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                name,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 4),
-              Text(
-                lastMessage,
-                style: const TextStyle(
-                  fontSize: 16,
-                  color: Colors.grey,
-                ),
-                maxLines: 1,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ChatRoomPage(
+              profile: profile,
+              name: name,
+            ),
           ),
-        ),
-        const SizedBox(width: 10),
-        Text(
-          time,
-          style: const TextStyle(
-            fontSize: 12,
-            height: 2,
-            fontWeight: FontWeight.w300,
-            color: Colors.grey,
+        );
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CircleAvatar(
+            backgroundImage: profile.image,
+            radius: 25,
           ),
-        ),
-      ],
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  name,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 4),
+                Text(
+                  lastMessage,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(width: 10),
+          Text(
+            time,
+            style: const TextStyle(
+              fontSize: 12,
+              height: 2,
+              fontWeight: FontWeight.w300,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
