8ed1348297402026dfc12a9d78f33cac5393b06a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-04 23:59:56 +0300

    Creating notification controller

---
+++ backend/app/Http/Controllers/NotificationController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Notification;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+        $notifications = Notification::all();
+        return response()->json($notifications);
+    }
+
+    public function show($id)
+    {
+        $notification = Notification::findOrFail($id);
+        return response()->json($notification);
+    }
+}
