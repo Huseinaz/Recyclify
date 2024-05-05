6017515c398d69011c9e4db778cae6ef2de188ef
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-05 23:49:29 +0300

    Creating admin middleware

--- backend/app/Http/Middleware/AdminMiddleware.php
+++ backend/app/Http/Middleware/AdminMiddleware.php
@@ -4,19 +4,25 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdminMiddleware
 {
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::user()->role_id == 1) {
-            return $next($request);
-        } else {
-            return response()->json([
-                'message' => 'Access Denied'
-            ]);
+        if(auth()->check()){
+            if (auth()->user()->role_id === 1) {
+                return $next($request);
+            } else {
+                return response()->json([
+                   'message' => 'Access Denied'
+                ]);
+            }
         }
     }
 }
