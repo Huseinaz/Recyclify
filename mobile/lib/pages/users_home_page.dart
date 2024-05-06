3af956912303169b3b73c396bd67a0ffcbe8f3b6
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-06 22:20:55 +0300

    Fetching container data

--- mobile/lib/pages/users_home_page.dart
+++ mobile/lib/pages/users_home_page.dart
@@ -1,9 +1,47 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:mobile/components/my_button.dart';
 import 'package:mobile/components/my_container.dart';
+import 'package:mobile/consts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class UserHomePage extends StatelessWidget {
+class UserHomePage extends StatefulWidget {
   const UserHomePage({super.key});
+
+  @override
+  _UserHomePageState createState() => _UserHomePageState();
+}
+
+class _UserHomePageState extends State<UserHomePage> {
+  List<Map<String, dynamic>> containers = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchContainers();
+  }
+
+  Future<void> fetchContainers() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString(KEY_ACCESS_TOKEN);
+
+    final response = await http.get(
+      Uri.parse('http://192.168.1.106:8000/api/containers'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        containers = List<Map<String, dynamic>>.from(jsonDecode(response.body)['containers']);
+      });
+    } else {
+      // Handle error
+      print('Failed to load containers data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,35 +87,12 @@ class UserHomePage extends StatelessWidget {
 
                 const SizedBox(height: 40),
 
-                const MyContainer(
-                  color: Colors.blue,
-                  type: 'Paper',
-                  percentage: 20,
-                ),
-
-                const SizedBox(height: 15),
-
-                const MyContainer(
-                  color: Colors.green,
-                  type: 'Glass',
-                  percentage: 20,
-                ),
-
-                const SizedBox(height: 15),
-
-                const MyContainer(
-                  color: Colors.yellow,
-                  type: 'Plastic',
-                  percentage: 20,
-                ),
-
-                const SizedBox(height: 15),
-
-                const MyContainer(
-                  color: Colors.red,
-                  type: 'Metal',
-                  percentage: 20,
-                ),
+                for (var container in containers)
+                  MyContainer(
+                    color: Colors.blue,
+                    type: container['type']['name'],
+                    percentage: container['capacity'],
+                  ),
 
                 const SizedBox(height: 40),
 
