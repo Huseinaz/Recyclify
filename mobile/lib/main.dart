f016e3db53b64abc401d3db3ba09014ed8195b0a
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-08 01:08:35 +0300

    Creating navigator key

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -3,12 +3,13 @@ import 'package:mobile/api/firebase_api.dart';
 import 'package:mobile/firebase_options.dart';
 import 'package:mobile/pages/d_home.dart';
 import 'package:mobile/pages/map_page.dart';
-import 'package:mobile/pages/tracking_page.dart';
+import 'package:mobile/pages/notifications_page.dart';
 import 'package:mobile/pages/u_home.dart';
 import 'package:mobile/pages/login_page.dart';
 import 'package:mobile/pages/signup_page.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,13 +26,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
+      navigatorKey: navigatorKey,
       routes: {
         '/login': (context) => LoginPage(),
         '/signup': (context) => SignupPage(),
         '/userhome': (context) => const UserPage(),
         '/driverhome': (context) => const DriverPage(),
         '/map': (context) => MapPage(),
-        '/tracking': (context) => const TrackingPage()
+        '/notifications': (context) => const NotificationsPage(),
       },
     );
   }
