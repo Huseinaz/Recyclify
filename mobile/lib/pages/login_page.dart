f6b552e5c72b855c73c1d7b93d043bef8cd3657d
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-08 23:50:04 +0300

    Updating login page

--- mobile/lib/pages/login_page.dart
+++ mobile/lib/pages/login_page.dart
@@ -7,43 +7,64 @@ import 'package:mobile/components/my_button.dart';
 import 'package:mobile/components/my_textfield.dart';
 import 'package:mobile/consts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class LoginPage extends StatelessWidget {
   LoginPage({super.key});
 
-  //text editing controllers
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
   void login(String email, password, BuildContext context) async {
-  try {
-    final response = await http.post(
-      Uri.parse('$HOST/login'),
-      body: {
-        'email': email,
-        'password': password,
-      },
-    );
-    if (response.statusCode == 200) {
-      final jsonData = json.decode(response.body);
-      final token = jsonData['authorisation']['token'];
+    try {
+      final response = await http.post(
+        Uri.parse('$HOST/login'),
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+      if (response.statusCode == 200) {
+        final jsonData = json.decode(response.body);
+        final token = jsonData['authorisation']['token'];
 
-      final prefs = await SharedPreferences.getInstance();
-      await prefs.setString(KEY_ACCESS_TOKEN, token);
+        final prefs = await SharedPreferences.getInstance();
+        await prefs.setString(KEY_ACCESS_TOKEN, token);
 
-      final roleId = jsonData['user']['role_id'];
-      if (roleId == 2) {
-        Navigator.pushNamed(context, '/userhome');
-      } else if (roleId == 3) {
-        Navigator.pushNamed(context, '/driverhome');
+        final roleId = jsonData['user']['role_id'];
+        if (roleId == 2) {
+          Navigator.pushNamed(context, '/userhome');
+        } else if (roleId == 3) {
+          Navigator.pushNamed(context, '/driverhome');
+        }
+      } else {
+        // Error handling
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Login Failed'),
+              content:
+                  const Text('Invalid email or password. Please try again.'),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
+              ],
+            );
+          },
+        );
       }
-    } else {
+    } catch (e) {
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text('Login Failed'),
-            content: const Text('Invalid email or password. Please try again.'),
+            title: const Text('Error'),
+            content: const Text('An error occurred. Please try again later.'),
             actions: [
               TextButton(
                 onPressed: () {
@@ -56,26 +77,7 @@ class LoginPage extends StatelessWidget {
         },
       );
     }
-  } catch (e) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Error'),
-          content: const Text('An error occurred. Please try again later.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      },
-    );
   }
-}
 
   @override
   Widget build(BuildContext context) {
@@ -85,18 +87,14 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: SingleChildScrollView(
             child: Column(
-              // mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const SizedBox(height: 40),
-
                 const Image(
                   image: AssetImage('assets/image/logo-bg-gray.png'),
                   height: 118,
                   width: 118,
                 ),
-
                 const SizedBox(height: 20),
-
                 const Text(
                   'Log In',
                   style: TextStyle(
@@ -104,38 +102,27 @@ class LoginPage extends StatelessWidget {
                     fontSize: 28,
                   ),
                 ),
-
                 const SizedBox(height: 40),
-
-                //email textfield
                 MyTextField(
                   controller: emailController,
                   hintText: 'Email',
                   obscureText: false,
                 ),
-
                 const SizedBox(height: 5),
-
-                //password textfield
                 MyTextField(
                   controller: passwordController,
                   hintText: 'Password',
                   obscureText: true,
                 ),
-
                 const SizedBox(height: 100),
-
-                //login button
                 MyButton(
                   onTap: () {
-                    login(emailController.text.toString(), passwordController.text.toString(), context);
+                    login(emailController.text.toString(),
+                        passwordController.text.toString(), context);
                   },
                   buttonText: 'Log In',
                 ),
-
                 const SizedBox(height: 3),
-
-                // or continue with
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 27),
                   child: Row(
@@ -162,18 +149,13 @@ class LoginPage extends StatelessWidget {
                     ],
                   ),
                 ),
-
                 const SizedBox(height: 3),
-
                 GoogleSignin(
                   onTap: () {
                     Navigator.pushNamed(context, '/driverhome');
                   },
                 ),
-
                 const SizedBox(height: 10),
-
-                //don't have an acount
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
