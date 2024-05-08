c8613c2b8de3b8efd704d8accc9b73e9b42f61a7
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-08 23:42:52 +0300

    Editing main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -8,12 +8,14 @@ import 'package:mobile/pages/u_home.dart';
 import 'package:mobile/pages/login_page.dart';
 import 'package:mobile/pages/signup_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
   await FirebaseApi().initNotifications();
   runApp(const MyApp());
 }
