e2404b4a5564eb5fb20179a3a45dd3bfc25910f9
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-09 22:16:07 +0300

    Editing constant

--- mobile/lib/consts.dart
+++ mobile/lib/consts.dart
@@ -2,4 +2,8 @@ const String GOOGLE_MAPS_API_KEY = "AIzaSyCBIcWVHKcRCsXyz6ouPPjPBa7pXIyTDg8";
 
 const String KEY_ACCESS_TOKEN = "token";
 
+const String KEY_USER_EMAIL = "email";
+
+const String KEY_USER_ID = "id";
+
 const String HOST = "http://192.168.1.106:8000/api";
