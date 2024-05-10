1ca1ce3b7e6f9e9c0000509fc94d4c72aa0abd18
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-11 00:27:21 +0300

    Editing driver request table name

--- backend/database/migrations/2024_05_10_212201_create_driver_requests_table.php
+++ backend/database/migrations/2024_05_10_212201_create_driver_requests_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('requests', function (Blueprint $table) {
+        Schema::create('driver_requests', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('requests');
+        Schema::dropIfExists('driver_requests');
     }
 };
