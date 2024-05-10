8e332af09c06a36ba3314955b2d65c16aedbad43
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-11 00:30:33 +0300

    Sending driver request

--- backend/app/Http/Controllers/DriverRequestController.php
+++ backend/app/Http/Controllers/DriverRequestController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\DriverRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DriverRequestController extends Controller
 {
-    //
+    public function driverRequest(Request $request)
+    {
+        $user = Auth::user();
+        
+        if ($user) {
+            $request = DriverRequest::create([
+                'user_id' => $user->id,
+                'status' => 'pending',
+            ]);
+    
+            return response()->json([
+                'message' => 'Request sent successfully',
+                'request' => $request
+            ], 200);
+        } else {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+    }
 }
