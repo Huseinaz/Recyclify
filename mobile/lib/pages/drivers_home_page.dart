14525ce01432b0636e39cedef3297faddd1f9fef
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-11 18:12:38 +0300

    Fetching driver requests

--- mobile/lib/pages/drivers_home_page.dart
+++ mobile/lib/pages/drivers_home_page.dart
@@ -1,11 +1,47 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile/components/request_container.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/consts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class DriverHomePage extends StatelessWidget {
-  DriverHomePage({super.key});
+class DriverHomePage extends StatefulWidget {
+  const DriverHomePage({super.key});
 
+  @override
+  State<DriverHomePage> createState() => _DriverHomePageState();
+}
+
+class _DriverHomePageState extends State<DriverHomePage> {
   final TextEditingController _textController = TextEditingController(text: '');
+  List<Map<String, dynamic>> driverRequests = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDriverRequests();
+  }
+
+  Future<void> fetchDriverRequests() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString(KEY_ACCESS_TOKEN);
+    
+    final response = await http.get(
+      Uri.parse('$HOST/viewRequests'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+    if (response.statusCode == 200) {
+      setState(() {
+        driverRequests = List<Map<String, dynamic>>.from(jsonDecode(response.body)['driver_request']);
+      });
+    } else {
+      print('Failed to load driver requests data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,30 +82,22 @@ class DriverHomePage extends StatelessWidget {
                 
                 const SizedBox(height: 20),
 
-                RequestContainer(
-                  name: 'John Doe',
-                  address: 'Beirut, Lebanon',
-                  onAccept: () {
-                    print('Accept request');
-                  },
-                  onReject: () {
-                    print('Reject request');
-                  },
+                for (var request in driverRequests)
+                Column(
+                  children: [
+                    RequestContainer(
+                      name: request['user']['first_name'] + ' ' + request['user']['last_name'],
+                      address: 'Beirut, Lebanon',
+                      onAccept: () {
+                        print('Accept request');
+                      },
+                      onReject: () {
+                        print('Reject request');
+                      },
+                    ),
+                    const SizedBox(height: 10), // Adjust the height as needed
+                  ],
                 ),
-
-                const SizedBox(height: 20),
-
-                RequestContainer(
-                  name: 'Jane Doe',
-                  address: 'Beirut, Lebanon',
-                  onAccept: () {
-                    print('Accept request');
-                  },
-                  onReject: () {
-                    print('Reject request');
-                  },
-                ),
-
               ],
             ),
           ),
