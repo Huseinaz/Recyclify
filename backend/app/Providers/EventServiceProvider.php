6584eed146dbd65ff4f6ac6d2e2e647146a31020
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-13 01:14:20 +0300

    Editing event service provider

--- backend/app/Providers/EventServiceProvider.php
+++ backend/app/Providers/EventServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Events\ContainerCapacityExceeded;
+use App\Listeners\SendCapacityNotification;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -25,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Event::listen(ContainerCapacityExceeded::class, [SendCapacityNotification::class, 'handle']);
     }
 
     /**
