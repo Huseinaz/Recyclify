806d885bf9bcfcd220644739e921fece0e22836b
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-14 14:43:52 +0300

    Fetching notification

--- mobile/lib/pages/notifications_page.dart
+++ mobile/lib/pages/notifications_page.dart
@@ -1,8 +1,46 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile/consts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mobile/components/notification_card.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
+
+  @override
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  List<Map<String, dynamic>> notifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications();
+  }
+
+  Future<void> fetchNotifications() async {
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString('KEY_ACCESS_TOKEN');
+
+    final response = await http.get(
+      Uri.parse('$HOST/notifications'),
+      headers: {
+        'Authorization': 'Bearer $token',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      setState(() {
+        notifications = List<Map<String, dynamic>>.from(jsonDecode(response.body));
+      });
+    } else {
+      print('Failed to load notifications');
+    }
+  }
 
  @override
    Widget build(BuildContext context) {
