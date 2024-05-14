4ef5a4730286808fdd845b2dbc29b61d11ce15f6
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-14 23:27:10 +0300

    Editing enum field in driver requests table

--- backend/database/migrations/2024_05_14_184049_update_enum_field_in_driver_requests_table.php
+++ backend/database/migrations/2024_05_14_184049_update_enum_field_in_driver_requests_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('driver_requests', function (Blueprint $table) {
-            //
+            DB::statement("ALTER TABLE driver_requests MODIFY status ENUM('Pending', 'Approved', 'Rejected', 'Done')");
         });
     }
 
