88ee0d30273903f5663cffbcccf6cda258d0ecd3
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-16 01:39:33 +0300

    Updating tracking page

--- mobile/lib/pages/tracking_page.dart
+++ mobile/lib/pages/tracking_page.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:mobile/components/tracking_container.dart';
 import 'package:http/http.dart' as http;
+import 'package:mobile/components/tracking_container.dart';
 import 'package:mobile/consts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -10,37 +9,49 @@ class TrackingPage extends StatefulWidget {
   const TrackingPage({super.key});
 
   @override
-  State<TrackingPage> createState() => _TrackingPageState();
+  _TrackingPageState createState() => _TrackingPageState();
 }
 
 class _TrackingPageState extends State<TrackingPage> {
-  List<Map<String, dynamic>> myRequest = [];
+  List<Map<String, dynamic>> requests = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchRequest();
+    fetchRequests();
   }
 
-  Future<void> fetchRequest() async {
-    final prefs = await SharedPreferences.getInstance();
-    final token = prefs.getString(KEY_ACCESS_TOKEN);
+  Future<void> fetchRequests() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final token = prefs.getString(KEY_ACCESS_TOKEN);
 
-    final response = await http.get(
-      Uri.parse('$HOST/myRequest'),
-      headers: {
-        'Authorization': 'Bearer $token',
-        'Content-Type': 'application/json',
-      },
-    );
+      final response = await http.get(
+        Uri.parse('$HOST/myRequest'),
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      );
 
-    if (response.statusCode == 200) {
+      if (response.statusCode == 200) {
+        final responseData = jsonDecode(response.body);
+        final List<Map<String, dynamic>> fetchedRequests =
+            List<Map<String, dynamic>>.from(responseData['driver_requests']);
+        fetchedRequests.sort((a, b) =>
+            a['user']['first_name'].compareTo(b['user']['first_name']));
+        setState(() {
+          requests = fetchedRequests;
+          isLoading = false;
+        });
+      } else {
+        print('Failed to load my requests');
+      }
+    } catch (error) {
+      print('Error fetching requests: $error');
       setState(() {
-        myRequest = List<Map<String, dynamic>>.from(
-            jsonDecode(response.body)['containers']);
+        isLoading = false;
       });
-    } else {
-      print('Failed to load my request');
     }
   }
 
@@ -64,36 +75,45 @@ class _TrackingPageState extends State<TrackingPage> {
           ),
         ),
       ),
-
       backgroundColor: const Color(0xFFF3F5F8),
-
       body: SafeArea(
-        child: Container(
-          margin: const EdgeInsets.only(left: 20, right: 20),
-          child: const SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                const SizedBox(height: 40),
-
-                TrackingContainer(
-                  name: 'John Doe',
-                  status: 'Pending',
-                  statusStyle: const TextStyle(
-                    color: Colors.green,
-                    decoration: TextDecoration.underline,
-                    decorationColor: Colors.green,
-                    fontWeight: FontWeight.bold,
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.green),
+                ),
+              )
+            : Container(
+                margin: const EdgeInsets.only(left: 20, right: 20),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 40),
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: requests.length,
+                        itemBuilder: (context, index) {
+                          final request = requests[index];
+                          return TrackingContainer(
+                            name: request['user']['first_name'] +
+                                ' ' +
+                                request['user']['last_name'],
+                            status: request['status'],
+                            statusStyle: TextStyle(
+                              color: request['status'] == 'Approved'
+                                  ? Colors.green
+                                  : Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          );
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                    ],
                   ),
                 ),
-
-                const SizedBox(height: 20),
-
-              ],
-            ),
-          ),
-        ),
+              ),
       ),
     );
   }
