178020cb46738c214a0fb5e86be3807f18b11499
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-16 20:33:09 +0300

    Updating container controller

--- backend/app/Http/Controllers/ContainerController.php
+++ backend/app/Http/Controllers/ContainerController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ContainerCapacityExceeded;
 use App\Models\Container;
 use Illuminate\Http\Request;
+use App\Notifications\FirebaseNotification;
 
 class ContainerController extends Controller
 {
@@ -16,6 +18,29 @@ class ContainerController extends Controller
     public function show($id)
     {
         $container = Container::findOrFail($id);
+        event(new ContainerCapacityExceeded($container));
         return response()->json($container);
     }
+
+    public function store()
+    {
+        $data = request()->validate([
+            'value'=>['required']
+        ]);
+        $container = Container::where('user_id', 4)->first(); 
+        
+        if ($container) {
+            $container->capacity = intval($data['value']);
+            
+            $container->save();
+            if($data['value'] == 100){
+                $container->user->notify(new FirebaseNotification("Your container is almost full."));
+            }
+            return response()->json(['message' => 'Container capacity updated successfully', 'container' => $container], 200);
+            event(new ContainerCapacityExceeded($container));
+        } else {
+            return response()->json(['error' => 'Container not found'], 404);
+        }
+    }
+
 }
