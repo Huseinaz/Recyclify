4c70f4d009cdb1dce38180cfcb9d8da7a6002ff1
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2024-05-19 16:26:04 +0300

    Updating messages page

--- mobile/lib/pages/messages_page.dart
+++ mobile/lib/pages/messages_page.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:mobile/consts.dart';
 import 'package:mobile/pages/chat_room_page.dart';
+import 'package:mobile/services/chat_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class MessagesPage extends StatefulWidget {
@@ -17,6 +18,7 @@ class MessagesPage extends StatefulWidget {
 class _MessagesPageState extends State<MessagesPage> {
   List<Map<String, dynamic>> users = [];
   bool isLoading = true;
+  final ChatService _chatService = ChatService();
 
   @override
   void initState() {
@@ -73,7 +75,12 @@ class _MessagesPageState extends State<MessagesPage> {
         ),
       ),
       backgroundColor: const Color(0xFFF3F5F8),
-      body: isLoading ? _buildLoader() : _buildUserList(),
+      body: isLoading
+          ? _buildLoader()
+          : Container(
+              margin: const EdgeInsets.only(top: 40),
+              child: _buildUserList(),
+            ),
     );
   }
 
@@ -122,28 +129,121 @@ class _MessagesPageState extends State<MessagesPage> {
           final currentUserEmail = prefs.getString(KEY_USER_EMAIL) ?? '';
 
           if (currentUserEmail != data['email']) {
-            return ListTile(
-              leading: CircleAvatar(
-                backgroundImage: profileImageUrl.isNotEmpty
-                    ? NetworkImage(profileImageUrl)
-                    : const AssetImage('assets/image/profile1.png')
-                        as ImageProvider,
-              ),
-              title: Text(data['name'],
-                  style: const TextStyle(
-                    fontSize: 18,
-                  )),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ChatRoomPage(
-                      receiverUserEmail: data['email'],
-                      receiverUserId: data['id'].toString(),
-                      receiverUserName: data['name'],
+            return FutureBuilder<DocumentSnapshot<Map<String, dynamic>>?>(
+              future: _chatService.getLastMessage(
+                  prefs.getInt(KEY_USER_ID).toString(), data['id'].toString()),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 5),
+                    child: ListTile(
+                      leading: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: CircleAvatar(
+                          backgroundImage: profileImageUrl.isNotEmpty
+                              ? NetworkImage(profileImageUrl)
+                              : const AssetImage('assets/image/profile1.png')
+                                  as ImageProvider,
+                        ),
+                      ),
+                      title: Text(
+                        data['name'],
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: const Text('Loading last message...'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatRoomPage(
+                              receiverUserEmail: data['email'],
+                              receiverUserId: data['id'].toString(),
+                              receiverUserName: data['name'],
+                            ),
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                );
+                  );
+                } else if (snapshot.hasError) {
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 5),
+                    child: ListTile(
+                      leading: SizedBox(
+                        width: 45,
+                        height: 45,
+                        child: CircleAvatar(
+                          backgroundImage: profileImageUrl.isNotEmpty
+                              ? NetworkImage(profileImageUrl)
+                              : const AssetImage('assets/image/profile1.png')
+                                  as ImageProvider,
+                        ),
+                      ),
+                      title: Text(
+                        data['name'],
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text('Error: ${snapshot.error}'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatRoomPage(
+                              receiverUserEmail: data['email'],
+                              receiverUserId: data['id'].toString(),
+                              receiverUserName: data['name'],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                } else {
+                  final lastMessage =
+                      snapshot.data != null && snapshot.data!.data() != null
+                          ? snapshot.data!.data()!['message'] as String
+                          : 'No messages yet';
+
+                  return Container(
+                    margin: const EdgeInsets.only(bottom: 5),
+                    child: ListTile(
+                      leading: SizedBox(
+                        width: 50,
+                        height: 50,
+                        child: CircleAvatar(
+                          backgroundImage: profileImageUrl.isNotEmpty
+                              ? NetworkImage(profileImageUrl)
+                              : const AssetImage('assets/image/profile1.png')
+                                  as ImageProvider,
+                        ),
+                      ),
+                      title: Text(
+                        data['name'],
+                        style: const TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                      subtitle: Text(lastMessage),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ChatRoomPage(
+                              receiverUserEmail: data['email'],
+                              receiverUserId: data['id'].toString(),
+                              receiverUserName: data['name'],
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  );
+                }
               },
             );
           } else {
